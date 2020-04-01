333f5d0dbb2d47ff2e8bb233f85f54f7fe43e32e
Natacia <taylorn7@gmatc.matc.edu>
2020-03-31 23:54:28 -0500

    Assignment 7

---
+++ Pickers/Pickers/DatePickerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DatePickerViewController.swift
+//  Pickers
+//
+//  Created by Natacia Taylor on 3/31/20.
+//  Copyright © 2020 Natacia Campbell. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let date = NSDate()
+        datePicker.setDate(date as Date, animated: false)
+    
+    }
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let date = datePicker.date
+        let message = "The date and time you selected is \(date)"
+        let alert = UIAlertController(title: "Date and Time Selected",
+                                      message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "That's so true!", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true,
+                completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pickers/Pickers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pickers
+//
+//  Created by Natacia Taylor on 3/31/20.
+//  Copyright © 2020 Natacia Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
